d8640bde7cecdef3cb56b4cdbab101c32a714a3e
4security <juanc.ortega@live.com>
2016-09-21 03:39:26 -0700

    Update ADResetPwd.ps1

--- ADResetPwd.ps1
+++ ADResetPwd.ps1
@@ -23,13 +23,13 @@ ForEach ($user in $users)
    false
  }  Else {
 # Set the default password for the current account
-    Get-ADUser $user | Set-ADAccountPassword -NewPassword $password -Reset
+  Get-ADUser $user | Set-ADAccountPassword -NewPassword $password -Reset
     
-    #If you need to set the property “Change password at next logon”, 
-    #leave the next alone. If not, comment the next line
-    Get-ADUser $user | Set-AdUser -ChangePasswordAtLogon $true
-    
-    Write-Host “Password has been reset for the user: $user”
+#If you need to set the property “Change password at next logon”, 
+#leave the next alone. If not, comment the next line
+  Get-ADUser $user | Set-AdUser -ChangePasswordAtLogon $true
+  
+  Write-Host “Password has been reset for the user: $user”
   
 
 } 
