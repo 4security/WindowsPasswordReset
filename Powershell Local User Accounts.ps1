48db16bec1c7aacbb73b6e73b584b0b8fe477840
4security <juanc.ortega@live.com>
2016-09-21 03:28:55 -0700

    Update Powershell Local User Accounts.ps1

--- Powershell Local User Accounts.ps1
+++ Powershell Local User Accounts.ps1
@@ -1,3 +1,5 @@
+#Credit - https://www.petri.com/how-to-change-user-password-with-powershell
+
 Get-WmiObject win32_useraccount | Foreach-Object {
 ([adsi](“WinNT://”+$_.caption).replace(“\”,”/”)).SetPassword(“FluxCapacitor!11-5-1955”)
 }
