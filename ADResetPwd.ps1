2e5b6bb0c493646bcd46cb2e3340d41690e2afe8
4security <juanc.ortega@live.com>
2016-09-21 03:38:08 -0700

    Update ADResetPwd.ps1

--- ADResetPwd.ps1
+++ ADResetPwd.ps1
@@ -1,4 +1,4 @@
-#
+# Credit: https://anandthearchitect.com/2014/02/27/active-directory-bulk-user-password-reset-by-powershell/
 # Script: ADResetPwd.ps1
 # Description: Reset the password for bulk number of users, and 
 # set the property to change passwrod required at next logon
@@ -11,13 +11,18 @@ Import-Module ActiveDirectory
 # Set the default password
 $password = ConvertTo-SecureString -AsPlainText “AwesomeP@ssw0rd” -Force 
  
+#Generate list of users
+wmic UserAccount get Name > UserList.txt
+
 # Get the list of accounts from the file on file
 # List the user names one per line
-$users = Get-Content -Path c:\MyScripts\UserList.txt
+$users = Get-Content -Path c:\UserList.txt
  
 ForEach ($user in $users) 
-{
-    # Set the default password for the current account
+ If ($user -eq "Name") {
+   false
+ }  Else {
+# Set the default password for the current account
     Get-ADUser $user | Set-ADAccountPassword -NewPassword $password -Reset
     
     #If you need to set the property “Change password at next logon”, 
@@ -25,6 +30,10 @@ ForEach ($user in $users)
     Get-ADUser $user | Set-AdUser -ChangePasswordAtLogon $true
     
     Write-Host “Password has been reset for the user: $user”
+  
+
+} 
+    
 }
 
 # ————- End ———–
